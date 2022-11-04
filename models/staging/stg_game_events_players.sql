4425eb4f7e530a039876b6174b853857cd710005
vampireweekend178@gmail.com <vampireweekend178@gmail.com>
2022-11-04 17:03:42 +0000

    Add game_events_players model stg

---
+++ models/staging/stg_game_events_players.sql
@@ -0,0 +1,24 @@
+Select  
+      partition_date,
+       {{ dbt_utils.surrogate_key(['game_id', 'event_id', 'period_s'])}} as play_id,
+      game_id,
+      game_season,
+      event_id,
+      players.value:player:id::string as player_id,
+      players.value:playerType::string as player_type
+  from (
+    Select 
+      partition_date,
+      JSON_EXTRACT:gameData:game:pk::string as game_id,
+      JSON_EXTRACT:gameData:game:season::string as game_season,
+      plays.value:about:eventId::int as event_id,
+      plays.value:players as players,
+      plays.value:about.ordinalNum::string as period_s
+
+    from {{source('SNOWFLAKE_RAW', 'RAW_NHL_GAME_DATA')}}, table(flatten(JSON_EXTRACT:liveData.plays.allPlays)) plays
+
+    {% if is_incremental() %}
+    where partition_date = date('{{ run_started_at }}')
+    {% endif %}
+
+  ), table(flatten(players)) players
