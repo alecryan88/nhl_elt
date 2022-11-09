0d1c8d597fe6a7d3968427aeb522d57987004400
vampireweekend178@gmail.com <vampireweekend178@gmail.com>
2022-11-09 13:44:47 +0000

    Add team name to metadata table for odds join

--- models/staging/stg_game_metadata.sql
+++ models/staging/stg_game_metadata.sql
@@ -4,7 +4,9 @@ Select
     JSON_EXTRACT:gameData:game:pk::string as game_id,
     JSON_EXTRACT:gameData:game:type::string as game_type,
     JSON_EXTRACT:gameData:teams:away.id as away_team_id,
+    JSON_EXTRACT:gameData:teams:away.name::string as away_team_name,
     JSON_EXTRACT:gameData:teams:home.id as home_team_id,
+    JSON_EXTRACT:gameData:teams:home.name::string as home_team_name,
     JSON_EXTRACT:gameData:datetime:dateTime::timestamp as game_start,
     JSON_EXTRACT:gameData:datetime:endDateTime::timestamp as game_end,
     JSON_EXTRACT:gameData:status:abstractGameState::string as game_state
